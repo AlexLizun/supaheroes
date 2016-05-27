a3da1197d5de64a6aa4f4a5491e089ced02312b4
Alex <laizon@rambler.ru>
2016-05-27 17:43:08 +0300

    no message

---
+++ src/com/company/Superhero.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+/**
+ * Created by alexis on 26.05.2016.
+ */
+
+public class Superhero {
+    String id;
+    String name;
+    String surname;
+    String login;
+    @Override
+    public String toString() {
+        return String.format(name + " and " + surname);
+    }
+// отправь на github, как сделаешь :)
+    //override tostring java - в гугле
+      //      и первая ссылка на stackoverflow :)
+}
