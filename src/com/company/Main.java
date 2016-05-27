0c6f176b42bd572531c81f13eb0234cdb1cda41a
Alex <laizon@rambler.ru>
2016-05-27 17:50:23 +0300

    no message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,40 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
+import java.lang.String;
+import java.util.ArrayList;
 
-public class Main{
+// вообще нужно импортить классы через import
+// но так как Superhero тоже в пакете com.company, то Main его "видит"
+public class Main {
 
-    public static void main(String[] args) {
-        try {
-            File file = new File("c:/Users/alexis/IdeaProjects/postrochno/users.txt");
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static final String FILE_PATH = "users.txt";
+    private static ArrayList<Superhero> ar = new ArrayList<Superhero>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(FILE_PATH));
+        while (in.hasNext()) {
+            String info[] = in.nextLine().split(" "); // кул
+            Superhero hero = new Superhero();
+            hero.id = info[0]; // здесь должна падать ошибка. У тебя String, пусть пока остаётся.
+            // Потом надо будет переделать на long - числовое значение, ведь это айдишник
+            hero.name = info[1];
+            hero.surname = info[2];
+            hero.login = info[3];
+            // В общем пока что норм)
+            ar.add(hero);
+            System.out.println(hero); // здесь будешь выводить hero вместо info[2]
+            // Только сначала переопредели .toString()
+
         }
+        int razmer = ar.size();
+       /* for (int i = 0; i <razmer ; i++) {
+            System.out.println(ar.get(i));
+        }*/
+
     }
 
-    public class Supaheroes{
-        int Id;
-        String Name;
-        String Surname;
-        String Login;
-    }
 }
